cc6562c804eb697a5c6ce5eb0e52150591c20828
Mihails Gilodo <47029668+gilodo@users.noreply.github.com>
2025-01-22 18:03:55 +0200

    футер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,7 +2,7 @@
 
 const sliderContainer = document.querySelector('.slider-container');
 const sliderBar = document.querySelector('.slider-bar');
-const imageRight = document.querySelector('.image-right');
+const imageRight = document.querySelector('.slider__screen--after');
 
 let isDragging = false;
 
@@ -30,35 +30,35 @@ document.addEventListener('mousemove', (e) => {
 });
 
 
-// КАРТА С ПОЛЬЗОВАТЕЛЬСКОЙ МЕТКОЙ
-// Инициализация карты
-function initMap() {
-  // Координаты центра карты
-  const mapCenter = { lat: 59.938631, lng: 30.323037 }; // Большая Конюшенная д 19/8, Санкт-Петербург
+// // КАРТА С ПОЛЬЗОВАТЕЛЬСКОЙ МЕТКОЙ
+// // Инициализация карты
+// function initMap() {
+//   // Координаты центра карты
+//   const mapCenter = { lat: 59.938631, lng: 30.323037 }; // Большая Конюшенная д 19/8, Санкт-Петербург
 
-  // Создание карты
-  const map = new google.maps.Map(document.getElementById('map'), {
-    center: mapCenter,
-    zoom: 14, // Масштаб карты
-  });
+//   // Создание карты
+//   const map = new google.maps.Map(document.getElementById('map'), {
+//     center: mapCenter,
+//     zoom: 14, // Масштаб карты
+//   });
 
-  // Параметры пользовательской метки
-  const customIcon = {
-    url: 'https://example.com/custom-icon.png', // URL вашей картинки
-    scaledSize: new google.maps.Size(50, 50),  // Размер изображения
-    origin: new google.maps.Point(0, 0),      // Точка начала
-    anchor: new google.maps.Point(25, 50),    // Точка привязки
-  };
+//   // Параметры пользовательской метки
+//   const customIcon = {
+//     url: 'https://example.com/custom-icon.png', // URL вашей картинки
+//     scaledSize: new google.maps.Size(50, 50),  // Размер изображения
+//     origin: new google.maps.Point(0, 0),      // Точка начала
+//     anchor: new google.maps.Point(25, 50),    // Точка привязки
+//   };
 
-  // Добавление метки на карту
-  new google.maps.Marker({
-    position: mapCenter, // Координаты метки
-    map: map,            // Ссылка на карту
-    icon: customIcon,    // Пользовательская иконка
-    title: 'Это пользовательская метка!', // Подсказка
-  });
-}
+//   // Добавление метки на карту
+//   new google.maps.Marker({
+//     position: mapCenter, // Координаты метки
+//     map: map,            // Ссылка на карту
+//     icon: customIcon,    // Пользовательская иконка
+//     title: 'Это пользовательская метка!', // Подсказка
+//   });
+// }
 
-// Запуск карты
-google.maps.event.addDomListener(window, 'load', initMap);
+// // Запуск карты
+// google.maps.event.addDomListener(window, 'load', initMap);
 
