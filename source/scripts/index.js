8e3d0ca44bedab69e72ff0652f9138dacb9ed4a3
Mihails Gilodo <47029668+gilodo@users.noreply.github.com>
2025-02-05 18:05:52 +0200

    + мобильная версия каталога

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -9,39 +9,3 @@ navToggle.addEventListener('click', ()=> {
   nav.classList.toggle('nav--opened');
 });
 
-// СЛАЙДЕР СО ШТОРКОЙ
-
-
-
-// // КАРТА С ПОЛЬЗОВАТЕЛЬСКОЙ МЕТКОЙ
-// // Инициализация карты
-// function initMap() {
-//   // Координаты центра карты
-//   const mapCenter = { lat: 59.938631, lng: 30.323037 }; // Большая Конюшенная д 19/8, Санкт-Петербург
-
-//   // Создание карты
-//   const map = new google.maps.Map(document.getElementById('map'), {
-//     center: mapCenter,
-//     zoom: 14, // Масштаб карты
-//   });
-
-//   // Параметры пользовательской метки
-//   const customIcon = {
-//     url: 'https://example.com/custom-icon.png', // URL вашей картинки
-//     scaledSize: new google.maps.Size(50, 50),  // Размер изображения
-//     origin: new google.maps.Point(0, 0),      // Точка начала
-//     anchor: new google.maps.Point(25, 50),    // Точка привязки
-//   };
-
-//   // Добавление метки на карту
-//   new google.maps.Marker({
-//     position: mapCenter, // Координаты метки
-//     map: map,            // Ссылка на карту
-//     icon: customIcon,    // Пользовательская иконка
-//     title: 'Это пользовательская метка!', // Подсказка
-//   });
-// }
-
-// // Запуск карты
-// google.maps.event.addDomListener(window, 'load', initMap);
-
